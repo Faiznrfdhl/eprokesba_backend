92f33161f5b5d851f9b9ed7e0d8ad9fe543e02f9
FzNrfdhl <68339451+Faiznrfdhl@users.noreply.github.com>
2025-12-13 19:37:46 +0700

    default

--- backend/settings.py
+++ backend/settings.py
@@ -24,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('DJANGO_SECRET_KEY')
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='').split(',')
 
-# Izinkan localhost, 127.0.0.1, dan semua IP di jaringan lokal (192.168.x.x)
-ALLOWED_HOSTS = ['*']
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'  # setelah login → ke dashboard (/)
 LOGOUT_REDIRECT_URL = '/login/'
